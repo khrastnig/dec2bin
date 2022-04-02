285e591bfc8ba54a54a9547bd9bf5db1ce5befc6
khrastnig <victy@gmx.at>
2022-04-02 15:46:08 +0200

    zwischenerfolg

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLCDNumber, QCheckBox
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLCDNumber, QCheckBox, QSlider
 from PyQt5.QtCore import Qt, QSize, QTimer
+#from Tkinter import *
 
 
 # Klasse für das Hauptfenster
@@ -10,40 +11,54 @@ class MyWindow(QMainWindow): # Beginn der Klase mit 2 Funktionen
 
         # Konstruktor von QMainWindow aufrufen
         super().__init__()
-        self.setMinimumSize(QSize(250, 120))    
-        self.setWindowTitle('Bit Representation') 
+        self.setMinimumSize(QSize(500, 200))    
+        self.setWindowTitle('Dezimal Binär Rechner Hrastnig') 
 
         wid = QWidget(self)
         self.setCentralWidget(wid)
 
-        vlayout = QVBoxLayout()  # Layout vertikale Box // es geht immer von aussen nach innen
-        wid.setLayout(vlayout)
+        
+        vbox= QVBoxLayout() 
+        wid.setLayout(vbox)
+        
+      # Slider + Label Anzeige Dezimalwert
+        sliderbox= QHBoxLayout() 
+        wid.setLayout(sliderbox)
+        self.slider = QSlider(Qt.Horizontal,wid)
+        self.slider.setRange(0, 15)
+        #bitlabel.clicked.connect(self.bin2dec)
+        self.label = QLabel('0') 
+        sliderbox.addWidget(self.slider)
+        sliderbox.addWidget(self.label)
+        
+        vbox.addLayout(sliderbox)
 
-        self.lcd_number = QLCDNumber(2, wid)
-        vlayout.addWidget(self.lcd_number)  #  nummernblock // die lcd_nummern werdenin die vlayout eingelesen
-
-        hlayout = QHBoxLayout()
-        vlayout.addLayout(hlayout)
-
-        #self.checkboxes = ["8":QCheckBox(wid), "4":QCheckBox(wid), "2":QCheckBox(wid), "1":QCheckBox(wid)] #ein array voller checkboxes
-        self.checkboxes = [QCheckBox(wid), QCheckBox(wid), QCheckBox(wid), QCheckBox(wid)] #ein array voller checkboxes
-        #for index, cb in enumerate(self.checkboxes):   #den index brauchen wir hier noch nicht, aber dann bei der Umrechnung
-        for cb in self.checkboxes:                     
-             hlayout.addWidget(cb)
-             cb.clicked.connect(self.bin2dec)
-             cb.setStyleSheet("background-color: rgb(127, 127, 127)")
-
-
-    def bin2dec(self):  # hier wird die Umrechnung vorgenommen // jede memberfunktion braucht das self!
-        value = 0
-        #numpy. flip(index) kehrt 
-        for index, cb in enumerate(self.checkboxes):
+       
+        #self.bitlabels = [QCheckBox(wid), QCheckBox(wid), QCheckBox(wid), QCheckBox(wid)] #ein array voller checkboxes
+        self.bitlabels = [QLabel("8"), QLabel("4"), QLabel("2"), QLabel("1")] #ein array voller checkboxes
+        # hier bitlabels erstellen      
+        bitbox = QHBoxLayout()
+        vbox.addLayout(bitbox)   
+        for bitlabel in self.bitlabels:
+            bitbox.addWidget(bitlabel)            
+            bitlabel.setStyleSheet("background-color: rgb(127, 127, 127);""selection-color: rgb(255, 0, 0);""text-aling: center;""border-width: 2px")
+            
+        
+            
+            
+    #def bin2dec(self):  # hier wird die Umrechnung vorgenommen // jede memberfunktion braucht das self!
+        #value = 0
+        #numpy. flip(index) reverse etc hat nicht funktioniert
+        #for self.slider
+        #for index, bitlabel in enumerate(self.bitlabels):
             #for cb in reversed(list(self.checkboxes)):                
-            if cb.isChecked():  #wenn angeklickt, dann berücksichtige sie
-                cb.setStyleSheet("background-color: rgb(255, 0, 0)")
-                exponents=4-index-1
-                value+=2**exponents        # um hochzalen zu rechnen, 2hoch3 =>  2**3 in p
-        self.lcd_number.display(value)
+            #if bitlabel.isChecked():  #wenn angeklickt, dann berücksichtige sie
+                #bbitlabel.setStyleSheet("background-color: rgb(255, 0, 0)")
+                #exponents=4-index-1  # self.checkboxes.len() sowie .count() und .length() hat nicht funktioniert
+                #value+=2**exponents        # um hochzahlen zu rechnen, 2hoch3 =>  2**3 in p
+                #showValue=str(value)
+        #self.label.setText(showValue)
+        #self.setLayout(value)
 
 
 app = QtWidgets.QApplication([]) # Start der Hauptfunktion
