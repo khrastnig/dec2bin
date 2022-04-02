022eeb4e537b089525f8f769b6fd4e1f986d5a12
khrastnig <victy@gmx.at>
2022-04-02 23:31:34 +0200

    fertig mit verbessertem layout

--- main.py
+++ main.py
@@ -31,8 +31,14 @@ class MyWindow(QMainWindow): # Beginn der Klase mit 2 Funktionen
       # Slider + Label Anzeige Dezimalwert
         sliderbox= QHBoxLayout() 
         wid.setLayout(sliderbox)
+        
         self.slider = QSlider(Qt.Horizontal,wid)
-        self.slider.setRange(0, 15)
+        self.slider.setRange(0, 15)        
+        self.slider.setStyleSheet("QSlider::handle:horizontal {height: 18px;}"
+            "QSlider::sub-page:horizontal {background-color:rgb(0,100,255);}"
+            "QSlider::handle:horizontal {background-color: white; border: 1px solid gray; border-radius: 4px; width: 18px;  margin: 0 ,0; }")  # die größeneinstellung des handler funktioniert irgendwie nicht
+        self.slider.setTickInterval(1)
+        self.slider.setTickPosition(QSlider.TicksBelow)
         self.slider.valueChanged[int].connect(self.changeValue)
         self.label = QLabel('0') 
         sliderbox.addWidget(self.slider)
@@ -48,12 +54,21 @@ class MyWindow(QMainWindow): # Beginn der Klase mit 2 Funktionen
 
 
 # hier bitlabels erstellen      
-        bitbox = QHBoxLayout()
-        vbox.addLayout(bitbox)   
+        bitbox = QHBoxLayout()       
+        vboxBitboxes= QVBoxLayout()
+        vboxBitboxes.addLayout(bitbox)
+        #vbox2.setFixedWidth(120)
+        vboxBitboxes.setAlignment(Qt.AlignCenter)
+        vbox.addLayout(vboxBitboxes)
         for index, bitlabel in enumerate(self.bitlabels):
             bitbox.addWidget(bitlabel)            
-            #bitlabel.setStyleSheet("background-color: rgb(127, 127, 127)")            
-    def changeValue(self, value):  # hier wird die Umrechnung vorgenommen // jede memberfunktion braucht das self!
+            bitlabel.setStyleSheet("background-color: rgb(127, 127, 127)")
+            bitlabel.setFixedWidth(20)
+            bitlabel.setFixedHeight(20)
+            bitlabel.setAlignment(Qt.AlignCenter)
+            
+            
+    def changeValue(self, value):  # hier nun die aufzurufende funktion
         showValue=str(value)
         #print(value)
         self.label.setText(showValue)
